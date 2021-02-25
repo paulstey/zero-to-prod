769ccbb3e3b1e64210b1773a68154eac0b529953
Paul Stey <paulstey@gmail.com>
2021-02-24 22:37:25 -0500

    implement basic health check

--- health_check/src/main.rs
+++ health_check/src/main.rs
@@ -1,8 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 
@@ -11,8 +11,7 @@ async fn greet(req: HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run()
